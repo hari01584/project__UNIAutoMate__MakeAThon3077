543b93fadd6c5a99f45b65e612ab06e03d058f63
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2021-03-07 00:05:35 +0530

    Notification Made Backend

--- app/admin.py
+++ app/admin.py
@@ -5,11 +5,9 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from core.models import complains, roomRequest, notifications
+from core.models import complains, roomRequest, notifications, medical
 from django.contrib import messages
 import datetime
-
-admin.site.register(complains)
 
 def accept(modeladmin, request, queryset):
     for i in list(queryset):
@@ -19,16 +17,22 @@ def accept(modeladmin, request, queryset):
         n.message = "Your Request For %s Has Been Accepted!" % (i.type,)
         n.time = datetime.datetime.now().strftime("%I:%M %p on %B %d, %Y")
         n.save()
-        #i.delete()
-
-
+        i.delete()
     messages.add_message(request, messages.INFO, 'Okay! Following request have been accepted!')
 
 accept.short_description = "Accept the Selected Orders"
 
 
 def decline(modeladmin, request, queryset):
-    messages.add_message(request, messages.ERROR, 'The selected requests are declined!!')
+    for i in list(queryset):
+        n = notifications()
+        n.forUser = i.user
+        n.title = i.type
+        n.message = "Your Request For %s Has Been Declined :((" % (i.type,)
+        n.time = datetime.datetime.now().strftime("%I:%M %p on %B %d, %Y")
+        n.save()
+        i.delete()
+    messages.add_message(request, messages.INFO, 'The selected requests are declined!!')
 
 decline.short_description = "Decline the Selected Orders"
 
@@ -39,6 +43,15 @@ class RoomAdmin(admin.ModelAdmin):
     list_display = ("name", "roomNo", "PhoneNo", "TimeCleaning")
     actions = [accept,decline]
 
+@admin.register(complains)
+class ComplainsAdmin(admin.ModelAdmin):
+    list_display = ("name", "roomno", "phoneno", "complaint")
+    actions = [accept,decline]
+
+@admin.register(medical)
+class MedicalAdmin(admin.ModelAdmin):
+    list_display = ("name", "roomno", "phoneno", "date", "problem", "time")
+    actions = [accept,decline]
 
 admin.site.site_header = "UAutoMate Admin Console"
-admin.site.unregister(Group)
+#admin.site.unregister(Group)
