93371bac68bda5a0c5b3ad56d76a88abaca1239b
Kamalpreet-3223 <ksingh7_be20@thapar.edu>
2021-03-06 23:38:19 +0530

    medical and complain pannel integrated to backend

--- core/models.py
+++ core/models.py
@@ -1,22 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class complains(models.Model):
-   user = models.ForeignKey(User, on_delete=models.CASCADE)
-   name= models.CharField(max_length = 50)
-   hostel = models.CharField(max_length = 50)
-   time = models.CharField(max_length = 50)
 
-   class Meta:
-      db_table = "complains"
+class complains(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    roomno = models.CharField(max_length=50)
+    phoneno = models.CharField(max_length=50)
+    complaint = models.CharField(max_length=500)
+
+    class Meta:
+        db_table = "complains"
+
+
+class medical(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    roomno = models.CharField(max_length=50)
+    phoneno = models.CharField(max_length=50)
+    date = models.CharField(max_length=50)
+    problem = models.CharField(max_length=500)
+    time = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = "medical"
 
 
 class roomRequest(models.Model):
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name= models.CharField(max_length = 50)
-    roomNo= models.CharField(max_length = 50)
-    PhoneNo= models.CharField(max_length = 50)
-    TimeCleaning= models.CharField(max_length = 50)
+    name = models.CharField(max_length=50)
+    roomNo = models.CharField(max_length=50)
+    PhoneNo = models.CharField(max_length=50)
+    TimeCleaning = models.CharField(max_length=50)
 
     class Meta:
         db_table = "roomRequest"
